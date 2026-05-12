c755719f7bbd296213050124617642925e092235
Herrrrr98 <110656504+Herrrrr98@users.noreply.github.com>
2026-05-12 10:26:10 +0800

    Add files via upload
    4/21-Modification

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ function curnt_time(bool){
     else return `${time.getHours()}:${minutes}`;
 };
 
+function timeToMin(timearr){
+    return timearr.split(":")[0] * 60 + timearr.split(":")[1];
+};
+
 var reqcount = 0;
 async function main(periodcallback){
         const response = await fetch(config.Requesting_Target_URL , {
@@ -33,7 +37,7 @@ async function main(periodcallback){
         console.log(`\n${curnt_time()}  Current refresh rate: ${config.timeouts.main_app/1000} sec.\n`+"Total requests:  " + `${reqcount}`.cyan);
         console.log(`Current Analytics Period: ${periodcallback}`)
         console.log(`Trains Online:     ${data.d.Result.length} in total`);
-        console.log(`Red Line: ${Rnum}/28  Useage: ${(Rnum/28)*100}%`.red + `\nOrange Line: ${Onum}/14 Useage: ${(Onum/14)*100}%`.yellow);
+        console.log(`Red Line: ${Rnum}/28  WorkLoad: ${(Rnum/28)*100}%`.red + `\nOrange Line: ${Onum}/14 WorkLoad: ${(Onum/14)*100}%`.yellow);
         for(let a = 0; a <= data.d.Result.length-1; a++){
             data.d.Result[a].LineNo === "O" ? 
             console.log(`Train No. ${ids[a]} at `+`${data.d.Result[a].Station}`.yellow)
@@ -60,13 +64,20 @@ async function DataAnalytics() {
                 var fileName = files[inner_repeat];
                 var file = fs.readFileSync(folderPath + '/'+ fileName, 'utf-8');
                 var contents = file.split("+");
-                //......(notice: contents[0] is empty)
-                console.log(`[[ ${folders[repeat].name + '/' + fileName} ]] \n ${contents} + "|" `);
+                contents.splice(0,1); //(contents[0] is empty, causing train&time would lose an data.)
+                var train = [], time = [];
+                for(let analytics_foreach = 0; analytics_foreach < contents.length; analytics_foreach++){
+                    train.push(contents[analytics_foreach].split(" | ")[1]);
+                    time.push(contents[analytics_foreach].split(" | ")[0]);
+                };
+                console.log(`[[ ${folders[repeat].name + '/' + fileName} ]] \n ${contents} \n${train} \n\n${time}`);
             }
         }
     }catch(e){
         console.error(e);
+        return false;
     }
+    return true;
 };
 
 async function app(){
@@ -100,7 +111,10 @@ async function app(){
         }};
         while(keep_alive){
         console.log('\x1B[2J\x1B[3J\x1B[H\x1Bc');
-        if(DataAnalytics_Period==0) DataAnalytics();
+        if(DataAnalytics_Period==0){
+            DataAnalytics_Period = config.timeouts.DataAnalytics_Period;
+            keep_alive = await DataAnalytics();
+        };
         keep_alive = await main(DataAnalytics_Period);
         if(keep_alive) await sleep(config.timeouts.main_app);
         DataAnalytics_Period--;
