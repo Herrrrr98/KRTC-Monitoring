eaaae80d626be0dba049cb85565e60171163036b
Herrrrr98 <110656504+Herrrrr98@users.noreply.github.com>
2026-05-19 12:13:04 +0800

    Add files via upload
    Preparation for Electron Development.

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
-const config = require('./config.json');
-const fs = require('fs');
 const color = require('colors');
+const fs = require('fs');
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const curnt_time = require('./function_curnt_time');
+const basic_data = require('./basic_data');
+const DataAnalytics = require('./DataAnalytics');
+const config = require('./config.json');
 
-
-
-var reqcount = 0;
-async function main(){
+async function app(){
+    var start_app = true;
+    var keep_alive = true;
+    if(config.whether_restore_when_starting){ 
         try{
-            const response = await fetch(config.Requesting_Target_URL , {
-                method: 'POST',
-                headers: {
-                    'User-Agent': config['Fetch-API_User-Agent_Alias'],
-                    'Content-Type': 'application/json',
-                },
-            });
-            const data = await response.json();
-            reqcount++;
-            var Rnum = 0, Onum = 0;
-            const ids = [];
-            for(let a = 0; a<= data.d.Result.length-1 ; a++){ // a = repeat
-                ids[a] = data.d.Result[a].TrainID;
-                if(Number(ids[a])<10) ids[a] = "0" + String(ids[a]);
-                String(data.d.Result[a].LineNo) === "O" ? Onum++ : Rnum++;
-            };
-            console.log(`\n${curnt_time()}  Current refresh rate: ${config.timeouts.main_app/1000} sec.\n`+"Total requests:  " + `${reqcount}`.cyan);
-            console.log(`Trains Online:     ${data.d.Result.length} in total`);
-            console.log(`Red Line: ${Rnum}/28  WorkLoad: ${(Rnum/28)*100}%`.red + `\nOrange Line: ${Onum}/14 WorkLoad: ${(Onum/14)*100}%`.yellow);
-            for(let a = 0; a <= data.d.Result.length-1; a++){
-                data.d.Result[a].LineNo === "O" ? 
-                console.log(`Train No. ${ids[a]} at `+`${data.d.Result[a].Station}`.yellow)
-                :
-                console.log(`Train No. ${ids[a]} at `+`${data.d.Result[a].Station}`.red);
-                fs.appendFileSync(`${config.src_Folder}/${curnt_time(true)}/id_${ids[a]}.txt`, `+ ${curnt_time()} | ${data.d.Result[a].Station} \n`);
-            };
+            fs.rmSync(`${config.src_Folder}`, {recursive: true});
+            fs.mkdirSync(`${config.src_Folder}`);
+            console.log(`${config.src_Folder}`.bgBlue + " is restored.  Please Wait for Starting".green);
+            await sleep(config.timeouts.restore_when_starting);
+    }catch(e){
+        console.log("Found Errors when Restoring".bgRed + `\n${e}`.red);
+        start_app = false;
+    }};
+    while(start_app){
+        try{    
+            fs.readdirSync(`${config.src_Folder}/${curnt_time(true)}`);
         }catch(e){
-            console.error(e);
-            return false;
-        };
-    return true;
-}
-
-module.exports = main;
+            console.warn(e);
+            try {
+                fs.mkdirSync(`${config.src_Folder}/${curnt_time(true)}`);
+                console.log("ERROR has been solved:".bgGreen);
+                console.log(`${config.src_Folder}/${curnt_time(true)}`.bgBlue + " is Created.  Please Wait for Starting".green);
+                keep_alive = true;
+                await sleep(config.timeouts.restore_when_starting);
+            }catch(err){
+                console.log("Found Errors when Creating New Folder".bgRed + `\n${err}`.red);
+                start_app = false;
+                keep_alive = false;
+        }};
+        //--------------------------------------------------------
+        while(keep_alive){
+        console.log('\x1B[2J\x1B[3J\x1B[H\x1Bc');
+        keep_alive = await basic_data();
+        if(keep_alive) await sleep(config.timeouts.main_app);
+        }
+    }
+};
+app().catch(console.error);
